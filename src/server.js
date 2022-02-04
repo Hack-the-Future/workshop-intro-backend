d782b892b1e9e615e8875ee408c85437f75daaf7
Jacob Zhang <jacobzhang132@gmail.com>
2022-02-03 23:29:16 -0500

    init express server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log('hello')
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import express from 'express'
+import itemRouter from './routers/itemRouter'
+
+const app = express()
+const port = 3000
+
+// Main url (localhost:3000)
+app.get('/', (req, res) => {
+  res.send('hi!')
+})
+
+// Router controllers
+app.use('/item', itemRouter)
+
+// Handle user requesting any other url
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
